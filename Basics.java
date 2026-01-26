6e0719582d365de4f90f18088be5d672a5e9ec44
クリシュナ <aan1zrk@gmail.com>
2026-01-26 20:34:24 +0530

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -2,6 +2,39 @@ package javagenesis;
 public class Basics{
   public static void main(String[]args){
     // I guess this are usually the first thing a programmer do while learning isnt it ? haha .. 
-    System.out.println("hey there , i hope you are doing well");
-    System.out.print("  Btw , My Name Is KrishnA And I Love Coding");
+    System.out.print("hey there , i hope you are doing well");
+    System.out.println("Btw , My Name Is KrishnA And I Love Coding");
+    System.out.println("lets get started..");
+    // this are the initial days of java learning so lets start from absolute basics 
+    // where we will understand the basics with some ease examples
+    // THIS IS SINGLE LINE COMMENT AND BELOW IS MULTIPLE LINE COMMENT
+    /* MULTI+
+              +PLE LINE
+                      COMMENTS
+    */
+    // day-01 : starting from data types ,java has various data types like byte int , Integer(lets understand it some other day),short etc 
+    byte byte1=127;
+    System.out.println(byte1);
+    short shortie=32767;
+    System.out.println(shortie);
+    int integer1=11111;
+    System.out.println(integer1);
+    long itIsLongType=99999999;
+    System.out.println(itIsLongType);
+    float itIsFloat=1.111f;
+    System.out.println(itIsFloat);
+    double doubledouble=1.99999999d;
+    System.out.println(doubledouble);
+    char charC0='1';
+    System.out.println(charC0);
+    char charC1=' ';
+    System.out.println(charC1);
+    char charC2='B';
+    System.out.println(charC2);
+    boolean isItSo= (1==0);// it returns true or false , 0 ( false ) or true (1) used to check conditions
+    System.out.println(isItSo);
+    // char cError=' 1 ';// it will show error cause char stores only 1 single character
+    /* This All Above Were Primitive Data Types,the questions is wdym by Primitive data type ? so , A primitive data type is a 
+    fundamental , built-in data type in a programming language that stores a single, indivisible value, acting as the basic 
+    building block for more complex types and not defined in terms of other type.*/
   }}

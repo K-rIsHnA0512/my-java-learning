5b3632c39845d2e47a7920983f0c6849f4bd2ac0
クリシュナ <aan1zrk@gmail.com>
2026-01-26 19:49:04 +0530

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,7 @@
+package javagenesis;
+public class Basics{
+  public static void main(String[]args){
+    // I guess this are usually the first thing a programmer do while learning isnt it ? haha .. 
+    System.out.println("hey there , i hope you are doing well");
+    System.out.print("  Btw , My Name Is KrishnA And I Love Coding");
+  }}

652f682fb66e3c47cc6f825837b1b1bb03abcb49
クリシュナ <aan1zrk@gmail.com>
2026-01-30 22:44:19 +0530

    Modify class visibility in Day6.java
    Changed 'public class' to 'class' for Day6.

--- Day6.java
+++ Day6.java
@@ -1,4 +1,4 @@
-public class Day6{
+class Day6{
   public static void main(String[] args){
     // today is day 6 lets continue where we left from , before starting let me tell iti know java , c , html  , css ( working on python , javascript rn ) but i am posting it daily just a bit everyday so i dont get fed up doing the same stuff 
   
